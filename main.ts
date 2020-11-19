87a544ddf5b817a80b693aa5fed305c9c811904d
ETiMe2021 <74692210+ETiMe2021@users.noreply.github.com>
2020-11-19 10:02:24 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+})
+basic.showNumber(0)
+basic.clearScreen()
+basic.forever(function () {
+	
+})
